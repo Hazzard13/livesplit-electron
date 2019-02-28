e7a875eea3c5c1857b18459ed21d74e65d10001f
Anthony Dragoni <contact@retroweeb.com>
2019-02-28 00:36:13 -0500

    Webview Fix for Process Lock, Zoom Ability

--- app/electron.js
+++ app/electron.js
@@ -1,11 +1,34 @@
-const {app, BrowserWindow} = require('electron');
+const {app, Menu, webFrame, BrowserWindow} = require('electron');
 const Store = require('electron-store');
 const store = new Store();
 let mainWindow;
+
 let bounds = {x:0, y:0, width:330, height:800};
 
 // Fixes it for Windows 10, noticed much more stable performance in OBS Linux as well.
 app.disableHardwareAcceleration();
+
+const menuTemplate = [
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin', accelerator:'=' },
+            { role: 'zoomout', accelerator:'-' }
+        ]
+    },
+    {
+        role: 'window',
+        submenu: [
+            { role: 'minimize' },
+            { role: 'close' }
+        ]
+    }
+];
+const menu = Menu.buildFromTemplate(menuTemplate);
 
 function createWindow () {
 
@@ -18,7 +41,6 @@ function createWindow () {
     }else {
         ico = cwd+"/icons/icon.png";
     } 
-    console.log(ico);
 
     // Check for Saved Bounds 
     if (typeof store.get('config') !== 'undefined' && typeof store.get('config').bounds !== 'undefined' && store.get('config').bounds !== null) {
@@ -34,16 +56,16 @@ function createWindow () {
         y:bounds.y,
         minWidth:50, 
         minHeight:50, 
-        menu:null,
         toolbar:false,
         maximizable:false,
         minimizable:false,
         fullscreen:false,
+        autoHideMenuBar:true,
         webPreferences:{nodeIntegration:true}
     };
 
     mainWindow = new BrowserWindow(windowConfig);
-    mainWindow.setMenu(null);
+    mainWindow.setMenu(menu);
 
     // Our Controller File
     mainWindow.loadFile('app/index.html');
@@ -55,6 +77,8 @@ function createWindow () {
     // Save our Window Bounds for Next Session
     mainWindow.on('closed', function () {
 
+        console.log('here');
+
         // Save Bounds on Exit
         store.set('config', {bounds:bounds});
 
@@ -62,6 +86,15 @@ function createWindow () {
 
     })
 
+    /*Shortcut.register(mainWindow, 'F1', () => {
+        let zoom = webFrame.getZoomFactor();
+        console.log(zoom);
+        webFrame.setZoomFactor(zoom+.25);
+    });*/
+
+    mainWindow.on('keyup', function(e) {
+        console.log(e);
+    });
     
 }
 
